931b307a0099524562e94e1e9207ee8cb3386ad6
chenzhou <chenzhou@thoughtworks.com>
2017-08-10 22:50:38 +0800

    [chen] add self handler : IHttpHandler & set config in web.config.

---
+++ myWebApp/src/myWeb/MyHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using System.Web;
+
+namespace myWeb
+{
+    public class MyHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            var requestUrl = context.Request.Url;
+            var content = Encoding.UTF8.GetBytes($"Hello {requestUrl}");
+            context.Request.ContentType = "text/plain";
+            context.Response.OutputStream.Write(content, 0, content.Length);
+        }
+
+        public bool IsReusable => true;
+    }
+}
